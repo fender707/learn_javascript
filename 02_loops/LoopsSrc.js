cce4fefabdf07c9a520c4b89498ef25d04e0b69d
fender707 <fender707@rambler.ru>
2015-04-12 21:04:28 +0300

    Added functions sum paramify

--- 02_loops/LoopsSrc.js
+++ 02_loops/LoopsSrc.js
@@ -4,3 +4,38 @@
       result += symbols;
     return result; 
   }
+
+  function join(array,glue) {
+    var result=""; 
+    glue = glue || "";
+    for(var i=0;i<array.length;i++){
+      result += array[i];
+      if(i!=array.length-1) 
+        result += glue;
+    }
+    return result;
+  }
+
+  function sum(array) {
+    var result=0;
+    for(var i=0;i<array.length;i++) {
+      result += array[i];
+    }
+    return result;
+  }
+ 
+  function paramify(hash) {
+    var result="";
+    var keys = Object.keys(hash).sort();
+    console.log(keys);
+    for(var index in keys){
+      var key=keys[index];  
+      result += key + "=" + hash[key] + "&";
+    }
+    if(result){
+      result = result.substring(0,result.length-1);
+     }
+    return result;
+   }
+   
+   
