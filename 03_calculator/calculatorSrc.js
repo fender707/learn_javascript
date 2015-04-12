f62d2de2a46cd26bbc7920fd754f04e692eb3767
fender707 <fender707@rambler.ru>
2015-04-12 22:00:12 +0300

    Added plus(),push(),pop()

--- 03_calculator/calculatorSrc.js
+++ 03_calculator/calculatorSrc.js
@@ -6,9 +6,25 @@
    Calculator.prototype = {
      value: function() {
        return this.stack[this.stack.length - 1];
-     }
+     },
+
+     add: function(x) {
+       this.push(x);
+       this.plus();
+     },
+     plus: function() {
+       this.stack.push(this.pop() + this.pop()); 
+     },
+    
+     push: function(value) {
+       this.stack.push(value)
+     },
+
+     pop: function() {
+       var number = this.stack.pop();
+       return number;
+
+     },
 
 
-
-
-   }
+   };
