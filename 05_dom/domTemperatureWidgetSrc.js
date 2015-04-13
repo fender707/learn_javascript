7b2e486e3fc98bf6180a2bbced820e78d0857fbc
fender707 <fender707@rambler.ru>
2015-04-13 15:17:56 +0300

    Added f2c function

--- 05_dom/domTemperatureWidgetSrc.js
+++ 05_dom/domTemperatureWidgetSrc.js
@@ -3,7 +3,11 @@
     var fahrenheit = this.widgetElement.getElementsByTagName('input')[0];
     var celcius = this.widgetElement.getElementsByTagName('span')[0];
     var convertButton = this.widgetElement.getElementsByTagName('input')[1];
-    
+     
+    function f2c(fahr) {
+      return (fahr-32) * 5/9;
+    }
+
     function convert() {
       var fahr = fahrenheit.value;
       var celci = f2c(fahr);
