5949c1edd56412ddc011b7fb62886df619eb8e00
fender707 <fender707@rambler.ru>
2015-04-13 18:43:16 +0300

    Added div element to object arena

--- 06_bouncing_ball/bouncingBallSrc.js
+++ 06_bouncing_ball/bouncingBallSrc.js
@@ -1,8 +1,18 @@
   
-  function BouncingBall(){
-    this.height = 10;
+  function BouncingBall(arena){
+
+   this.div = document.createElement('div');
+   arena.appendChild(this.div);
+   this.height = 10;
     this.width = 10;
     this.x=0;
     this.y=0;    
     this.color = "#FF0000";
-  }
+
+    this.div.style.height = this.height.toString() + "px";
+    this.div.style.width = this.width.toString() + "px";
+    this.div.style.left = this.x.toString() + "px";
+    this.div.style.top = this.y.toString() + "px";
+    this.div.style.backgroundColor = this.color;
+    
+}
