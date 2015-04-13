b3aa064f0e0f1e841440da747e79c20751fbb3e8
fender707 <fender707@rambler.ru>
2015-04-13 15:13:00 +0300

    Added convert function, need to add f2c function

--- 05_dom/domTemperatureWidgetSrc.js
+++ 05_dom/domTemperatureWidgetSrc.js
@@ -3,8 +3,16 @@
     var fahrenheit = this.widgetElement.getElementsByTagName('input')[0];
     var celcius = this.widgetElement.getElementsByTagName('span')[0];
     var convertButton = this.widgetElement.getElementsByTagName('input')[1];
+    
+    function convert() {
+      var fahr = fahrenheit.value;
+      var celci = f2c(fahr);
+      celcius.innerHTML = celci;
+      
+    }
+
     this.fahrenheit = fahrenheit;
     this.celcius = celcius;
     this.convertButton = convertButton;
-    
+    this.convert = convert;
   }
