4782858ef3fcd80e108c715f0c6bfdf126f07a6d
fender707 <fender707@rambler.ru>
2015-04-13 13:39:29 +0300

    Added function gradient

--- 05_dom/colorSrc.js
+++ 05_dom/colorSrc.js
@@ -12,6 +12,15 @@
      this.blue.toHex();
     },
 
+    gradient: function(othcolor,persent) {
+      if(persent < 0) persent = 0;
+      if(persent > 1) persent = 1;
+      return new Color(((othcolor.red - this.red) * persent + this.red), 
+             ((othcolor.green - this.green)*persent + this.green), 
+             ((othcolor.blue - this.blue) * persent + this.blue));
+
+    }
+
   }
 
   Number.prototype.toHex = function() {
@@ -20,3 +29,5 @@
       hex = "0" +hex;
     return hex;
   }
+
+  
