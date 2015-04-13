5cc5c964d3b5b8b2aec6304f8ecd135427ce360e
fender707 <fender707@rambler.ru>
2015-04-13 09:02:08 +0300

    Working with Temperature object

--- 04_temperature/temperatureSrc.js
+++ 04_temperature/temperatureSrc.js
@@ -6,3 +6,21 @@
   function c2f(value) {
     return value*9.0/5+32;
   }
+
+  function Temperature(DegrFahr) {
+    this.fahrenheit = function() {
+      return DegrFahr;
+    }
+
+    this.celcius = function() {
+      return (DegrFahr-32)*5/9;
+    }
+
+    this.setFahrenheit = function(fahren) {
+      DegrFahr = fahren;
+    }
+
+    this.setCelcius = function(celsi) {
+      DegrFahr = celsi * (9/5) + 32;
+    }
+  }
