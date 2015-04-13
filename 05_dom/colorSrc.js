4302541a3a67efde5bd079ce824591f34a5be390
fender707 <fender707@rambler.ru>
2015-04-13 11:20:40 +0300

    Added Number.prototype, toHex() function

--- 05_dom/colorSrc.js
+++ 05_dom/colorSrc.js
@@ -3,3 +3,20 @@
     this.green = green;
     this.blue = blue;
   }
+
+  Color.prototype = {
+    toHex: function() {
+     return "#" +
+     this.red.toHex() + 
+     this.green.toHex() + 
+     this.blue.toHex();
+    },
+
+  }
+
+  Number.prototype.toHex = function() {
+    var hex = this.toString(16);
+    if(hex.length == 1)
+      hex = "0" +hex;
+    return hex;
+  }
